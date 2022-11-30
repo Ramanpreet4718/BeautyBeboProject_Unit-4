6104349a29ec4a2e2794cdcd2151f9508f9b841a
Saurabh Pratap Singh <surabpratapsingh1116@gmail.com>
2022-11-30 13:38:23 +0530

    project

---
+++ objects/fregrance.js
@@ -0,0 +1,182 @@
+var fregranceData  = [
+    {
+      image_url:
+        "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/u/n/untitled-1_6.jpg",
+      name: "KamaSutra Thrill Deodorant (150ml each)",
+      price: 396.00,
+    },
+    {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/u/n/untitled-1_6.jpg",
+        name: "kamaSutra Blaze Deodorant - Pack Of 2 (150ml each)",
+        price: 396.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/u/n/untitled-1_6.jpg",
+        name: "KamaSutra Urge Power Series Perfume Spray- Pack Of 2 (135ml each)",
+        price: 396.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/u/n/untitled-1_6.jpg",
+        name: "KamaSutra Woman Sparkle Perfume Body Spray (150ml each) ",
+        price: 398.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/u/n/untitled-1_6.jpg",
+        name: "KamaSutra Woman Enigma Perfume Body Spray - Pack Of 2 (150ml each) ",
+        price: 398.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/m/e/me-deodorant.jpg",
+        name: "Plum Bodylovin' Feelin' So Rose Body Misz (150ml) ",
+        price: 457.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/m/e/me-deodorant.jpg",
+        name: "Plum Bodylovin' Orchid You Not Body Misz (150ml) ",
+        price: 473.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/m/e/me-deodorant.jpg",
+        name: " Plum Bodylovin' Trippin' Mimosas Body Mist (150ml)",
+        price: 473.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/m/e/me-deodorant.jpg",
+        name: " Mamaearth Eau De Parfum For A Fragrance As Unique As You (50ml)",
+        price: 629.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/m/e/me-deodorant.jpg ",
+        name: " Mamaearth ME Deo For A Scent That’s Unique To You (120ml)",
+        price: 299.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/s/8/s8.jpg",
+        name: " Calvin Klein Eternity For Men Eau De Parfum (50ml)",
+        price:3468.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/f/o/fo21.jpg",
+        name: "Fogg Bleu Series Island Fragrance Body Spray (120ml) ",
+        price: 255.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/f/o/fo20.jpg",
+        name: "Fogg Rush Fragrance Body Spray (150ml) ",
+        price:230.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/f/o/fo15.jpg",
+        name: " Fogg Scent Intensio Men Fragrance Body Spray (100ml)",
+        price:539.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/f/o/fo13.jpg",
+        name: " Fogg Master Cedar Body Spray (150ml)",
+        price: 275.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/f/o/fo12.jpg",
+        name: "Fogg Black Fresh Aromatic Fragrance Body Spray (150ml)",
+        price:250.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/f/o/fo10.jpg",
+        name: "Fogg Master Agar Body Spray (150ml)",
+        price: 275.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/f/o/fo8.jpg",
+        name: "Fogg Scent Xpressio Men Fragrance Body Spray (100ml)",
+        price: 539.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/f/o/fo7.jpg",
+        name: "Fogg Dynamic Fragrance Body Spray (150ml)",
+        price: 250.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/f/o/fo6.jpg",
+        name: "Fogg Scent Xtremo Men Fragrance Body Spray (100ml)",
+        price: 539.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/f/o/fo3.jpg",
+        name: "Fogg Master Oak Body Spray (150ml)",
+        price: 275.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/r/r/rr.jpg",
+        name: "Fogg Scent Beautiful Secret Women Fragrance Body Spray (100ml)",
+        price: 539.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/e/5/e55.jpg",
+        name: "Engage Yin Eau De Parfum For Men (90ml)",
+        price: 459.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/d/2/d29.jpg",
+        name: "Denver Sporting Club Rider Deo (165ml)",
+        price: 179.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/d/2/d28.jpg",
+        name: "Denver Hamilton Caliber Perfume - 100ml (100ml)",
+        price: 448.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/d/2/d27.jpg",
+        name: "Denver Sporting Club - Victor Deo - 165ml (165ml)",
+        price: 179.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/d/2/d26.png",
+        name: "Denver Hamilton Imperial Perfume (60ml)",
+        price: 257.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/d/2/d25.jpg",
+        name: "Denver Royal Oud Deo - Hamilton (150ml)",
+        price: 179.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/d/2/d24.jpg",
+        name: "Denver Hamilton Imperial Perfume - 100ml (100ml)",
+        price: 448.00,
+      },
+      {
+        image_url:
+          "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/p/l/pl34.jpg",
+        name: "Plum Green Tea Clear Skin Specialist Duo",
+        price: 1663.00,
+      },
+  ];
